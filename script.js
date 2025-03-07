c30bec9ef8956e33df912915b8c9c069181db8e4
Jacob <96797676+Jacob-Sehl@users.noreply.github.com>
2025-03-07 13:53:49 -0500

    Add DOM variables to script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+/**
+ * CALCULATOR APP
+ *  Creates a function GUI calculator in the browser with basic operations based on user input
+ * 
+ * MAIN FUNCTIONS:
+ *  ADD
+ *  SUBTRACT
+ *  MULTIPLY
+ *  DIVIDE
+ *  OPERATE
+ * 
+ * UI INTERACTION / CSS MODIFICATION
+ *  MOUSOVER GLOW
+ *      #1 event handling
+ *      #2 modify css
+ *  CLICK RESPONSE
+ *      #1 event handling
+ *      #2 modify css
+ *  DISPLAY NUMBERS
+ *      #1 Button display
+ *      #2 Operation result
+ *  CLEAR NUMBERS
+ */
+
+// DOM VARIABLES
+const calcScreen = document.querySelector(".screen");
+const clearButton = document.querySelector(".clear");
+const deleteButton = document.querySelector(".delete");
+const numberButtons = document.querySelectorAll(".num");
+const operatorButtons = document.querySelectorAll(".operator");
+
+function operate(){
+
+}
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+function glow(){
+
+}
+
+function display(){
+
+}
+
+function clear(){
+
+}
